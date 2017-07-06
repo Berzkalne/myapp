522d95f423124ad4ad01dbfd44ea8e832da7cdd6
Berzkalne <zaneberzkalne@gmail.com>
2017-07-06 05:31:44 +0300

    changes added

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,6 @@ module Myapp
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-    config.i18n.available_locales = [:lv, :en ]
+    config.i18n.available_locales = [:lv, :en]
   end
 end
