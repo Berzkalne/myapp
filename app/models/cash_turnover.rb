9534e1023b4a8d655f6c38636b62f555e6520eba
Berzkalne <zaneberzkalne@gmail.com>
2017-07-30 08:23:50 +0300

    cash turnover statistics done. menu updated.

--- app/models/cash_turnover.rb
+++ app/models/cash_turnover.rb
@@ -5,4 +5,15 @@ class CashTurnover < ActiveRecord::Base
     calculator = FinalPriceCalculator.new(self)
     self.price = calculator.calculates_final_price
   end
+
+  def statistics_height()
+    if self.price < 0
+      self.price.abs/20
+    elsif self.price > 0
+      self.price/20 + 150
+    else
+      self.price = 150
+    end
+  end
+
 end
